85453c5b8fa98b1656c597d42f9dbdccac365547
AndrejNemec <andrejnemec007@gmail.com>
2018-09-10 18:03:31 +0200

    BedWars init.. commit

--- src/main/java/net/LukAd/BedWars/Listeners/PlayerListener.java
+++ src/main/java/net/LukAd/BedWars/Listeners/PlayerListener.java
@@ -1,7 +1,15 @@
 package net.LukAd.BedWars.Listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerListener implements Listener {
+
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+
+    }
 
 }
