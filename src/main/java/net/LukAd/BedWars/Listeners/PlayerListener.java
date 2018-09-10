9ece12ed1616fc20700bbe04342f7b6342cf5f6f
Lukzon <luk.hanak592@gmail.com>
2018-09-10 18:04:18 +0200

    Lukzon

--- src/main/java/net/LukAd/BedWars/Listeners/PlayerListener.java
+++ src/main/java/net/LukAd/BedWars/Listeners/PlayerListener.java
@@ -1,5 +1,6 @@
 package net.LukAd.BedWars.Listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,7 +10,7 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-
+        Player player = e.getPlayer();
     }
 
 }
